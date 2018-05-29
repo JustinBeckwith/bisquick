4bf73700757398de630737a95e92dfdb678c7be3
Justin Beckwith <justin.beckwith@gmail.com>
2018-05-29 10:53:22 -0700

    add usage instructions

--- BigQuery.cs
+++ BigQuery.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using Google.Cloud.BigQuery.V2;
 using Google.Apis.Bigquery.v2.Data;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System.IO;
+using System.Linq;
 
 namespace bisquick
 {
@@ -14,16 +16,19 @@ namespace bisquick
     protected static string datasetName = "bisquick";
     protected static string tableName = "issues";
 
-    public BigQuery(string projectId) {
+    public BigQuery(string projectId)
+    {
       this.client = BigQueryClient.Create(projectId);
     }
 
-    public void init() {
+    public void init()
+    {
       var dataset = this.client.GetOrCreateDataset(datasetName);
       var file = File.OpenText("tableSchema.json");
       var serializer = new JsonSerializer();
       var schema = (TableSchema)serializer.Deserialize(file, typeof(TableSchema));
-      var options = new CreateTableOptions {
+      var options = new CreateTableOptions
+      {
         TimePartitioning = TimePartition.CreateDailyPartitioning(expiration: null)
       };
       var table = dataset.GetOrCreateTable(tableName, schema, null, options);
@@ -32,10 +37,42 @@ namespace bisquick
     public void insertIssues(List<Issue> issues)
     {
       var table = client.GetTable(datasetName, tableName);
-      foreach (var issue in issues) {
-        var json = JsonConvert.SerializeObject(issue);
+      foreach (var issue in issues)
+      {
+        var json = JsonConvert.SerializeObject(issue, new ItemStateConverter(typeof(StringEnum<ItemState>)));
         Console.WriteLine(json);
       }
     }
   }
+
+  public class ItemStateConverter : JsonConverter
+  {
+    private readonly Type[] _types;
+
+    public ItemStateConverter(params Type[] types)
+    {
+      _types = types;
+    }
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+      //throw new NotImplementedException("Not done yet!");
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+      throw new NotImplementedException("Unnecessary because CanRead is false. The type will skip the converter.");
+    }
+
+    public override bool CanRead
+    {
+      get { return false; }
+    }
+
+    public override bool CanConvert(Type objectType)
+    {
+      return _types.Any(t => t == objectType);
+    }
+  }
+
 }
--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # 🥞 bisquick
 > A fancy little utility for synchronizing GitHub issues with BigQuery.
 
+![bisquick](https://i.imgur.com/uS1kH7i.gif?noredirect)
+
 ## Usage
-**COMING SOON**
+
+First, you need to set up your environment.  Bisquick expects the `BISQUICK_PROJECT_ID` and `BISQUICK_GITHUB_TOKEN` environment settings to be set. You're probably also going to want to `gcloud config set project $PROJECT_ID`.
+
+To setup your project, the `init` command will create a dataset, and a table with the correct schema.
+
+```sh
+$ dotnet run init
+```
+
+After that, you can run the `sync` command to read all the GitHub issues, and save them to a partitioned BigQuery table:
+
+```sh
+$ dotnet run sync
+```
 
 ## License
 [Apache 2.0](LICENSE.md)
